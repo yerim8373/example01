2909430b425b385786e013b97e6555d1970947e0
yerim8373 <yerim8373@naver.com>
2023-12-17 15:02:22 +0900

    init

---
+++ src/main/java/com/example/example01/Example01Application.java
@@ -0,0 +1,13 @@
+package com.example.example01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Example01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Example01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/example01/Example01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.example01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Example01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

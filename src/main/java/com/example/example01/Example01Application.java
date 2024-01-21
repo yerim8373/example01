e56f59d2a1276011652e1d0efcd6634ee38f2255
yerim8373 <yerim8373@naver.com>
2024-01-21 19:42:37 +0900

    feature : Hello

--- src/main/java/com/example/example01/Example01Application.java
+++ src/main/java/com/example/example01/Example01Application.java
@@ -2,10 +2,15 @@ package com.example.example01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class Example01Application {
 
+    @GetMapping("/")
+    public String hello(){
+        return "Hello Spring Boot";
+    }
     public static void main(String[] args) {
         SpringApplication.run(Example01Application.class, args);
     }
